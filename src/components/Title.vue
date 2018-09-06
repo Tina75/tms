8777538f45fdd12937589708d33ac11a4d011a99
马友胜 <march99999@gmail.com>
2018-09-06 09:25:39 +0800

    升级iview3.1.0 新增表格组件

---
+++ src/components/Title.vue
@@ -0,0 +1,20 @@
+<template>
+  <h3 class="header-h3">
+    <div class="header-h3-title">
+      <slot></slot>
+    </div>
+  </h3>
+</template>
+
+<style lang="stylus" scoped>
+.header-h3
+  border-bottom 1px dashed #CBCED3
+  color #333333
+  padding 12px 0
+  margin 24px 0
+  &-title
+    border-left 4px solid #00A4BD
+    padding-left 20px
+    font-size 16px
+
+</style>
