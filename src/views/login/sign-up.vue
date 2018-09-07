bf5064dda47f71bf8f4ae9b3c6c7604a8c92a498
Troy <1045132927@qq.com>
2018-09-07 15:32:36 +0800

    新增登录模块&修改登录页面路由

---
+++ src/views/login/sign-up.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="login-con" @keydown.enter="handleSubmit">
+    <Card :bordered="false">
+      <p slot="title">
+        <Icon type="log-in"/>
+        欢迎注册
+      </p>
+      <div class="form-con">
+        <Form ref="loginForm" :model="form" :rules="rules">
+          <FormItem prop="telephone">
+            <Input v-model="form.telephone" placeholder="请输入用户手机号">
+            <Icon slot="prefix" type="ios-contact" />
+            </Input>
+          </FormItem>
+          <FormItem prop="captcha">
+            <Row>
+              <Col :span="13">
+              <Input v-model="form.captcha" type="text" placeholder="请输入验证码">
+              <Icon slot="prefix" type="ios-chatbubbles" />
+                </Input>
+              </Col>
+              <Col :span="9" :offset="1">
+              <Button  :disabled="!captchaEnable" @click="getCaptcha">{{captchaEnable?`&nbsp;&nbsp;&nbsp;获取验证码&nbsp;&nbsp;&nbsp;`:intervalSeconds+'秒后可重试'}}</Button>
+                <!-- <Button :size="buttonSize" >90s后重新获取</Button> -->
+              </Col>
+            </Row>
+          </FormItem>
+          <FormItem>
+            <i-button type="primary" long @click="login">注册</i-button>
+          </FormItem>
+          <FormItem style="text-align: right;">
+            <router-link to="/" style="margin-right: 20px;">用户登录</router-link>
+            <router-link to="/login/find-back">忘记密码</router-link>
+          </FormItem>
+        </Form>
+        <p class="login-tip">运达达运输管理系统</p>
+      </div>
+    </Card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignUp',
+  metaInfo: {
+    title: '注册'
+  },
+  data () {
+    return {
+      captchaEnable: true,
+      timer: null,
+      intervalSeconds: 90,
+      form: {
+        telephone: '',
+        captcha: ''
+      },
+      rules: {
+        telephone: [
+          { required: true, message: '用户名不能为空', trigger: 'blur' },
+          { max: 11, min: 11, trigger: 'blur', message: '手机格式有误' }
+        ],
+        captcha: [
+          { required: true, message: '验证码不能为空', trigger: 'blur' },
+          { max: 6, min: 6, trigger: 'blur', message: '验证码格式有误' }
+        ]
+      }
+    }
+  },
+  methods: {
+    handleSubmit () {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.login()
+        }
+      })
+    }
+  }
+}
+</script>
