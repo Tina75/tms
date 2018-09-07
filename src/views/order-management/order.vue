feb51cbaf3a07e3e74d5e7772bce926aa8a12eec
xuanfei <fei.xuan@ymm56.com>
2018-09-07 10:57:07 +0800

    订单模块新增

---
+++ src/views/order-management/order.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>订单管理</div>
+</template>
+
+<script>
+import BasePage from '@/basic/BasePage'
+export default {
+  name: 'order',
+
+  components: {},
+  mixins: [ BasePage ],
+  metaInfo: { title: '订单管理' },
+  data () {
+    return {
+    }
+  },
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+}
+</script>
+<style lang='stylus' scoped>
+</style>
---
+++ src/views/order-management/receipt.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>回单管理</div>
+</template>
+
+<script>
+import BasePage from '@/basic/BasePage'
+export default {
+  name: 'receipt',
+
+  components: {},
+  mixins: [ BasePage ],
+  metaInfo: { title: '回单管理' },
+  data () {
+    return {
+    }
+  },
+
+  computed: {},
+
+  mounted () {},
+
+  methods: {}
+}
+</script>
+<style lang='stylus' scoped>
+</style>
