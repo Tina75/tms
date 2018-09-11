f234afc8c53bf5e7143b4b15d85f267548051698
王军杰 <junjie.wang@ymm56.com>
2018-09-11 15:52:31 +0800

    公司管理-UI整理

---
+++ src/views/company-manage/edited-staff-dialog.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="dialog">
+    <Modal v-model="visibale" :mask-closable="false" width="360">
+      <p slot="header" style="text-align:center">
+        <span>{{title}}</span>
+      </p>
+      <Form ref="formModal" :model="formModal" :rules="rulesModal" :label-width="120" label-position="left" style="height: 200px;">
+        <FormItem label="员工姓名：" prop="name">
+          <Input v-model="formModal.name" placeholder="请输入员工姓名"></Input>
+        </FormItem>
+        <FormItem label="手机号：" prop="phone">
+          <Input v-model="formModal.phone" placeholder="请输入员工姓名"></Input>
+        </FormItem>
+        <FormItem label="角色：" prop="select">
+          <Select v-model="formModal.select" clearable>
+            <Option
+              v-for="item in selectList"
+              :value="item.name"
+              :key="item.id">
+              {{ item.name }}
+            </Option>
+          </Select>
+        </FormItem>
+      </Form>
+      <div slot="footer">
+        <Button  type="primary"  @click="save">确定</Button>
+        <Button  type="default"  @click="close">取消</Button>
+      </div>
+    </Modal>
+  </div>
+</template>
+
+<script>
+import Server from '@/libs/js/server'
+import BaseDialog from '@/basic/BaseDialog'
+export default {
+  name: 'editUser',
+  mixins: [BaseDialog],
+  data () {
+    return {
+      formModal: {
+        name: '',
+        phone: '',
+        select: ''
+      },
+      selectList: [{
+        name: '全部',
+        id: '1'
+      }, {
+        name: '管理员',
+        id: '2'
+      }, {
+        name: '录入员',
+        id: '3'
+      }],
+      rulesModal: {
+        name: [
+          { required: true, message: '请输入员工姓名', trigger: 'blur' }
+        ],
+        phone: [
+          { required: true, message: '请输入手机号', trigger: 'blur' }
+        ],
+        select: [
+          { required: true, message: '请选择角色', trigger: 'blur' }
+        ]
+      },
+      visibale: true
+    }
+  },
+  watch: {
+    visibale: function (val) {
+      !val && this.close()
+    }
+  },
+  mounted: function () {
+    this.formModal = Object.assign({}, this.formData)
+  },
+  methods: {
+    save () {
+      this.$refs['formModal'].validate((valid) => {
+        if (valid) {
+          Server({
+            url: 'user/update',
+            method: 'post',
+            data: this.formModal
+          }).then(() => {
+            this.ok()
+            this.visibale = false
+          })
+        }
+      })
+    }
+  }
+
+}
+
+</script>
+<style lang='stylus' scoped>
+.dialog
+  p
+    text-align center
+</style>
