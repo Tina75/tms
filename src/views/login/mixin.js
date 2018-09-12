03cd140ee4f2505a40f18764e0075779a12b918d
徐亮 <liang.xu@ymm56.com>
2018-09-12 08:58:40 +0800

    登录模块页面与逻辑

---
+++ src/views/login/mixin.js
@@ -0,0 +1,149 @@
+import Server from '@/libs/js/server'
+
+let timer
+let waitingTime = 5
+
+export default {
+  replace: true,
+  data () {
+    return {
+      captchaImage: '', // 图片验证码
+      captchaEnable: true,
+      intervalSeconds: waitingTime
+    }
+  },
+  methods: {
+    // 输入框校验
+    validate (type, { extraRules, done } = {}) {
+      switch (type) {
+        case 'phone':
+          if (!this.form.phone.length) {
+            this.$Message.error('手机号不能为空')
+            return false
+          } else if (this.form.phone.length !== 11 || this.form.phone[0] !== '1' || isNaN(Number(this.form.phone))) {
+            this.$Message.error('手机号格式不正确，请重输')
+            return false
+          }
+          break
+        case 'password':
+          if (!this.form.password.length) {
+            this.$Message.error('密码不能为空')
+            return false
+          }
+          break
+        case 'confirmPassword':
+          if (!this.form.confirmPassword.length) {
+            this.$Message.error('确认密码不能为空')
+            return false
+          }
+          if (this.form.password !== this.form.confirmPassword) {
+            this.$Message.error('前后两次密码不一致')
+            return false
+          }
+          break
+        case 'captchaCode':
+          if (!this.form.captchaCode.length) {
+            this.$Message.error('图形验证码不能为空')
+            return false
+          }
+          break
+        case 'smsCode':
+          if (!this.form.smsCode.length) {
+            this.$Message.error('验证码不能为空')
+            return false
+          }
+          break
+        case 'name':
+          if (!this.form.name.length) {
+            this.$Message.error('公司名不能为空')
+            return false
+          }
+          break
+        case 'userName':
+          if (!this.form.userName.length) {
+            this.$Message.error('联系人不能为空')
+            return false
+          }
+          break
+        case 'address':
+          if (!this.form.address.length) {
+            this.$Message.error('公司地址不能为空')
+            return false
+          }
+          if (this.form.address.length > 40 || this.form.address.length < 5) {
+            this.$Message.error('公司地址不能少于5个字也不能超过40个字')
+            return false
+          }
+          break
+        case 'cityId':
+          if (!this.form.cityId.length) {
+            this.$Message.error('省市区不能为空')
+            return false
+          }
+          break
+        default: break
+      }
+      let valid = true
+      if (extraRules) valid = extraRules()
+      done && done()
+      return valid
+    },
+
+    // 校验密码-添加设置密码时的位数规则
+    inputBlurWithPw () {
+      return this.validate('password', {
+        extraRules: () => {
+          if (this.form.password.length > 16 || this.form.password.length < 6) {
+            this.$Message.error('密码格式不正确，至少6位，至多16位')
+            return false
+          }
+          return true
+        }
+      })
+    },
+
+    // 获取图片验证码
+    getCaptcha () {
+      Server({
+        url: '/user/captcha',
+        method: 'get'
+      }).then(res => {
+        this.captchaImage = res.data.data
+      }).catch(err => console.error(err))
+    },
+
+    // 发送手机验证码
+    sendSMS (url) {
+      const data = {
+        phone: this.form.phone,
+        captchaCode: this.form.captchaCode
+      }
+      for (let key in data) {
+        if (!this.validate(key)) return
+      }
+      Server({
+        url,
+        method: 'get',
+        data
+      }).then(res => {
+        this.$Message.success('短信验证码已发送至手机，请注意查收')
+        this.captchaEnable = false
+        timer = setInterval(() => {
+          if (this.intervalSeconds > 1) this.intervalSeconds--
+          else {
+            this.intervalSeconds = waitingTime
+            this.captchaEnable = true
+            clearInterval(timer)
+          }
+        }, 1000)
+      }).catch(err => console.error(err))
+    },
+
+    // 输入框失焦
+    inputBlur (type) {
+      if (this.currentFocus !== undefined) this.currentFocus = ''
+      this.validate(type)
+    }
+  }
+
+}
--- src/views/login/sign-up.vue
+++ src/views/login/sign-up.vue
@@ -1,120 +1,223 @@
 <template>
-  <div class="login-con" @keydown.enter="handleSubmit">
-    <Card :bordered="false">
-      <p slot="title">
-        <Icon type="log-in"/>
-        欢迎注册
-      </p>
-      <div class="form-con">
-        <Form ref="loginForm" :model="form" :rules="rules">
-          <FormItem prop="companyName">
-            <Input v-model="form.companyName" placeholder="请输入公司名">
-            <Icon slot="prefix" type="ios-contact" />
-            </Input>
-          </FormItem>
-          <FormItem prop="location">
-            <Input v-model="form.location" placeholder="请输入所在省市区">
-            <Icon slot="prefix" type="ios-contact" />
-            </Input>
-          </FormItem>
-          <FormItem prop="companyAddress">
-            <Input v-model="form.companyAddress" placeholder="请输入公司地址">
-            <Icon slot="prefix" type="ios-contact" />
-            </Input>
-          </FormItem>
-          <FormItem prop="telephone">
-            <Input v-model="form.telephone" placeholder="请输入手机号">
-            <Icon slot="prefix" type="ios-contact" />
-            </Input>
-          </FormItem>
-          <FormItem prop="telCaptcha">
-            <Row>
-              <Col :span="13">
-              <Input v-model="form.telCaptcha" type="text" placeholder="请输入验证码">
-              <Icon slot="prefix" type="ios-chatbubbles" />
-                </Input>
-              </Col>
-              <Col :span="9" :offset="1">
-              <Button  :disabled="!captchaEnable">{{captchaEnable?`&nbsp;&nbsp;&nbsp;获取验证码&nbsp;&nbsp;&nbsp;`:intervalSeconds+'秒后可重试'}}</Button>
-                <!-- <Button :size="buttonSize" >90s后重新获取</Button> -->
-              </Col>
-            </Row>
-          </FormItem>
+  <div class="form-body" @keydown.enter="nextStep">
+    <Card :bordered="false" style="height: 100%;">
+      <div class="form-title">注册账号</div>
+
+      <ul class="form-step">
+        <li v-for="(item, key) in stepList"
+            :key="key" class="form-step-item">
+          <div :class="{'form-step-item-circle-active': step === key}" class="form-step-item-circle"><span>{{ key + 1 }}</span></div>
+          <p :class="{'form-step-item-tip-active': step === key}" class="form-step-item-tip">{{item}}</p>
+        </li>
+      </ul>
+
+      <div class="form-content">
+
+        <Form ref="loginForm" :model="form">
+          <!-- step 1 -->
+          <template v-if="step === 0">
+            <FormItem prop="phone">
+              <Input v-model="form.phone" :maxlength="11" placeholder="输入手机号"
+                     @on-blur="inputBlur('phone')" />
+            </FormItem>
+
+            <FormItem prop="captchaCode">
+              <Input v-model="form.captchaCode" class="form-captcha-input" type="text" placeholder="输入验证码"
+                     @on-blur="inputBlur('captchaCode')" />
+              <div class="form-captcha">
+                <img :src="captchaImage" class="form-captcha-img" >
+              </div>
+            </FormItem>
+
+            <FormItem prop="smsCode">
+              <Row>
+                <Col :span="14">
+                <Input v-model="form.smsCode" type="text" placeholder="输入手机验证码"
+                       @on-blur="inputBlur('smsCode')" />
+                </Col>
+                <Col :span="9" :offset="1">
+                <Button :disabled="!captchaEnable"
+                        long
+                        @click="sendSMS('/user/sms')">{{captchaEnable?`&nbsp;&nbsp;&nbsp;获取验证码&nbsp;&nbsp;&nbsp;`:intervalSeconds+'秒后可重试'}}</Button>
+                </Col>
+              </Row>
+            </FormItem>
+          </template>
+
+          <!-- step 2 -->
+          <template v-if="step === 1">
+            <FormItem prop="userName">
+              <Input v-model="form.userName" placeholder="输入联系人姓名"
+                     @on-blur="inputBlur('userName')" />
+            </FormItem>
+            <FormItem prop="name">
+              <Input v-model="form.name" :maxlength="25" placeholder="输入公司名称"
+                     @on-blur="inputBlur('name')" />
+            </FormItem>
+            <FormItem prop="cityId">
+              <Cascader :data="[]" v-model="form.cityId" placeholder="选择省/市/区"
+                        @on-blur="inputBlur('cityId')"></Cascader>
+            </FormItem>
+            <FormItem prop="address">
+              <Input v-model="form.address" :maxlength="40" placeholder="输入公司详细地址"
+                     @on-blur="inputBlur('address')" />
+            </FormItem>
+          </template>
+
+          <!-- step 3 -->
+          <template v-if="step === 2">
+            <FormItem prop="password">
+              <Input v-model="form.password" :maxlength="16" type="password" placeholder="设置登录密码"
+                     @on-blur="inputBlurWithPw" />
+            </FormItem>
+            <FormItem prop="confirmPassword">
+              <Input v-model="form.confirmPassword" :maxlength="16" type="password" placeholder="再次输入密码"
+                     @on-blur="inputBlur('confirmPassword')" />
+            </FormItem>
+          </template>
+
           <FormItem>
-            <i-button type="primary" long>注册</i-button>
-          </FormItem>
-          <FormItem style="text-align: right;">
-            <router-link to="/" style="margin-right: 20px;">用户登录</router-link>
-            <router-link to="/login/find-back">忘记密码</router-link>
+            <Button class="form-button" type="primary" long
+                    @click="nextStep">{{step === 2 ? '立即注册' : '下一步'}}</Button>
+            <p style="text-align: right;">已有账号？<router-link to="/">请登录></router-link></p>
           </FormItem>
         </Form>
-        <p class="login-tip">运达达运输管理系统</p>
       </div>
     </Card>
   </div>
 </template>
 
 <script>
+import Server from '@/libs/js/server'
+import mixin from './mixin'
+
 export default {
   name: 'SignUp',
+  mixins: [ mixin ],
   metaInfo: {
-    title: '注册'
+    title: '注册账号'
   },
   data () {
     return {
-      captchaEnable: true,
-      timer: null,
-      intervalSeconds: 90,
+      step: 0,
+      stepList: ['验证手机号', '填写账号信息', '注册成功'],
+
       form: {
-        companyName: '',
-        companyAddress: '',
-        location: '',
-        telephone: '',
-        imgCaptcha: '',
-        telCaptcha: '',
+        phone: '',
+        captchaCode: '',
+        smsCode: '',
         password: '',
-        ensurePassword: ''
-      },
-      rules: {
-        companyName: [
-          { required: true, message: '公司名不能为空', trigger: 'blur' },
-          { max: 25, trigger: 'blur', message: '公司名不能超过25个字' }
-        ],
-        location: [
-          { required: true, message: '省市区不能为空', trigger: 'blur' }
-        ],
-        companyAddress: [
-          { required: true, message: '公司地址不能为空', trigger: 'blur' },
-          { max: 40, min: 5, trigger: 'blur', message: '公司地址不能少于5个字也不能超过40个字' }
-        ],
-        telephone: [
-          { required: true, message: '手机号不能为空', trigger: 'blur' },
-          { max: 11, min: 11, trigger: 'blur', message: '手机号格式不正确，请重输' }
-        ],
-        telCaptcha: [
-          { required: true, message: '验证码不能为空', trigger: 'blur' },
-          { max: 6, min: 6, trigger: 'blur', message: '验证码格式有误' }
-        ],
-        imgCaptcha: [
-          { required: true, message: '图形验证码不能为空', trigger: 'blur' }
-        ],
-        password: [
-          { required: true, message: '密码不能为空', trigger: 'blur' }
-        ],
-        ensurePassword: [
-          { required: true, message: '确认密码不能为空', trigger: 'blur' }
-        ]
+        confirmPassword: '',
+        name: '', // 公司名称
+        userName: '', // 联系人姓名
+        address: '', // 公司地址
+        location: [], // 所在省市区
+        cityId: ''
       }
     }
   },
   methods: {
-    handleSubmit () {
-      this.$refs.loginForm.validate(valid => {
-        if (valid) {
-          this.login()
+    nextStep () {
+      let validParams
+      if (this.step === 0) {
+        validParams = ['phone', 'captchaCode', 'smsCode']
+      } else if (this.step === 1) {
+        validParams = ['name', 'userName', 'cityId', 'address']
+      } else {
+        validParams = this.form
+      }
+
+      for (let key in validParams) {
+        if (key !== 'password') {
+          if (!this.validate(key)) return
+        } else {
+          if (!this.inputBlurWithPw(key)) return
         }
-      })
+      }
+
+      if (this.step !== 2) {
+        this.step++
+        return
+      }
+      Server({
+        url: '/user/register',
+        method: 'post',
+        data: validParams
+      }).then(res => {
+        this.$Message.success('注册成功')
+        setTimeout(() => {
+          this.$router.push('/')
+        }, 2000)
+      }).catch(err => console.error(err))
     }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+  .form-body
+    width 800px
+    height 520px
+    left 50%
+    top 50%
+    transform translate(-50%, -50%)
+
+    .form-content
+      width 300px
+      margin 24px auto
+
+    .form-step
+      position relative
+      display flex
+      align-items space-between
+      width 500px
+      margin 30px auto
+
+      &:before
+        content ""
+        position absolute
+        display block
+        width 70%
+        height 4px
+        left 15%
+        top 35%
+        border-top 2px dashed #9DA1B0
+        z-index 0
+
+      &-item
+        flex 1
+        display flex
+        flex-direction column
+        align-items center
+        position relative
+        z-index 1
+
+        &-circle
+          width 50px
+          height 50px
+          background #FFFFFF
+
+          span
+            display block
+            width 30px
+            height 30px
+            margin 10px
+            border-radius 50%
+            border 1px solid #9DA1B0
+            text-align center
+            line-height 30px
+            font-size 20px
+            color #9DA1B0
+
+          &-active span
+            color #FFFFFF
+            background #00A4BD
+            border-color #00A4BD
+
+        &-tip
+          font-size 12px
+          color #9DA1B0
+
+          &-active
+            color #00A4BD
+</style>
