15989c8442fe0bf70aa0fbcbac99a23abd80f553
马友胜 <yousheng.ma@ymm56.com>
2018-09-12 15:17:59 +0800

    图标组件

---
+++ src/components/Icon.vue
@@ -0,0 +1,24 @@
+<template>
+  <i :class="classes"></i>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+      default: ''
+    }
+  },
+  computed: {
+    classes () {
+      return ['icon', 'icon-family', this.type ? `icon-${this.type}` : '']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
