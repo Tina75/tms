770dc2a5e620dbdce0e6f3c5c0f223bae63b5f21
高婷 <ting.gao@ymm56.com>
2018-09-19 11:32:31 +0800

    修改联调bug

--- src/views/client/client.js
+++ src/views/client/client.js
@@ -1,10 +1,11 @@
 import server from '../../libs/js/server'
 // 基本变量
 export const CODE = 10000
+export const CAR = /(^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(([0-9]{5}[DF]$)|([DF][A-HJ-NP-Z0-9][0-9]{4}$)))|(^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳]{1}$)/
 // 发货方列表
 export function consignerList (data) {
   return server({
-    url: '/consigner/list',
+    url: '/consigner/page',
     method: 'GET',
     data: data
   })
