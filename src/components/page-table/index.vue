169d7da8338837f6d134d7c70ac032a30a4408dd
马友胜 <yousheng.ma@ymm56.com>
2018-10-09 11:20:15 +0800

    1. 已维护的货物信息，修改完数量，同步更新重量，体积和价值 2. page-table 显示分页，条件唯一

--- src/components/page-table/index.vue
+++ src/components/page-table/index.vue
@@ -30,7 +30,7 @@
         <slot name="header"></slot>
       </div>
     </Table>
-    <div v-if="showPagination && !ltPageSize" class="page-table__footer-pagination">
+    <div v-if="showPagination" class="page-table__footer-pagination">
       <div class="page-table__footer-pagination-fr">
         <Page
           :total="pagination.totalCount"
@@ -293,12 +293,6 @@ export default {
         const { pageSize, pageNo } = this.pagination
         return this.dataSource.slice((pageNo - 1) * pageSize, pageNo * pageSize)
       }
-    },
-    /**
-     * 总数据少于pageSize 分页不予显示
-     */
-    ltPageSize () {
-      return this.pagination.totalCount <= this.pagination.pageSize
     }
   },
   watch: {
