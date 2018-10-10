d70792d7320d698e42dbad425888681715553393
徐亮 <liang.xu@ymm56.com>
2018-10-10 14:25:17 +0800

    运输管理修改tab通过query切换的方式

--- src/views/transport/transportMixin.js
+++ src/views/transport/transportMixin.js
@@ -43,8 +43,8 @@ export default {
     if (columns) this.extraColumns = JSON.parse(columns)
 
     let tab
-    if (this.$route.query.tab) {
-      tab = this.$route.query.tab
+    if (this.$route.query.tab && this.$route.query.tab < this.tabList.length) {
+      tab = this.tabList[this.$route.query.tab].name
       window.sessionStorage.setItem('TABHEADER_' + this.tabType, tab)
     } else {
       tab = window.sessionStorage['TABHEADER_' + this.tabType]
