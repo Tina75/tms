c36a4e0865a4e4203f47e81ef12ab7c3de4f1c5f
xuanfei <fei.xuan@ymm56.com>
2018-10-15 14:50:31 +0800

    送货调度取消默认始发地和目的地

--- src/views/order-management/dialog/dispatch.vue
+++ src/views/order-management/dialog/dispatch.vue
@@ -239,10 +239,10 @@ export default {
   },
   mounted: function () {
     // modify:20181013 by 马友胜 加上第一条数据的始发城市和目的城市
-    setTimeout(() => {
-      this.send.start = this.id[0].start
-      this.send.end = this.id[0].end
-    }, 0)
+    // setTimeout(() => {
+    //   this.send.start = this.id[0].start
+    //   this.send.end = this.id[0].end
+    // }, 0)
   },
 
   methods: {
