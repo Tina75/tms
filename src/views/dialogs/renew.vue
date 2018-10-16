5bb319a92ccc4d2ea7fab6d7d4bd24871d00cf0e
季康 <jik@ymm56.com>
2018-10-16 16:14:28 +0800

    修改会员提示文案

--- src/views/dialogs/renew.vue
+++ src/views/dialogs/renew.vue
@@ -2,16 +2,16 @@
   <Modal v-model="visiable" :mask-closable="false" width="440" @on-visible-change="close">
     <p slot="header" style="text-align:center;font-size:17px">{{title}}</p>
     <br>
-    <b class="dialog-title">本版本将于<a>{{expirationTime | datetime}}</a>到期，到期后您将无法使用本软件</b>
+    <b class="dialog-title">您当前账户免费试用期将于<a>{{expirationTime | datetime}}</a>截止，到期后如需继续使用，请联系我们咨询购买。</b>
 
-    <p class="i-mt-40">您可以选择延长使用本软件，延长之后，您将拥有如下权益：</p>
+    <p class="i-mt-40">购买软件后，您即可享受以下保障：</p>
     <ol class="dialog-content">
-      <li>软件的使用期限将增加365天</li>
-      <li>将有专人为您提供服务</li>
-      <li>系统更加稳定</li>
+      <li>更强大、更稳定的运输管理功能</li>
+      <li>物流专家一对一的服务支持</li>
+      <li>技术工程师24小时待命确保售后无忧</li>
     </ol>
     <div class="dialog-footer">
-      如需延长有效期，请联系客服
+      使用咨询、购买洽谈，请联系我们。
       <p><a ><i class="icon font_family icon-ico-call"></i>025-66606056</a></p>
     </div>
     <div slot="footer" style="text-align:center">
