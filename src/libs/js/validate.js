7f0500417cd92997bc1e43e719bb551812422574
马友胜 <yousheng.ma@ymm56.com>
2018-10-24 15:09:05 +0800

    1. 费用校验bug修复

--- src/libs/js/validate.js
+++ src/libs/js/validate.js
@@ -5,6 +5,13 @@ const validator = {
    */
   phone (value) {
     return /^1[0-9]{10}$/.test(value)
+  },
+  /**
+   * 费用一律限制整数位9位，精确2位小数
+   * @param {number} value
+   */
+  fee (value) {
+    return /^[0-9]{0,9}(?:\.\d{1,2})?$/.test(value)
   }
 }
 
