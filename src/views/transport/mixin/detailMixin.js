9373d5465718986db5c1a19f277d28e4a5cd9392
徐亮 <liang.xu@ymm56.com>
2018-10-25 10:54:17 +0800

    修改样式&bugs

--- src/views/transport/mixin/detailMixin.js
+++ src/views/transport/mixin/detailMixin.js
@@ -194,7 +194,7 @@ export default {
         this.$Message.error('请输入正确的车牌号')
         return false
       }
-      if (this.settlementType === '1' && !this.$refs.$settlement.validate()) return false
+      if (this.settlementType === '1' && !this.$refs.$payInfo.validate()) return false
 
       return true
     }
