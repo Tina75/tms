57d07b096c08552eeca12d0e9ca47d2eba6ccc83
chentianwei <chentw@ymm56.com>
2018-10-30 20:17:45 +0800

    ga & 兼容性

--- src/components/SelectInputForCity.vue
+++ src/components/SelectInputForCity.vue
@@ -50,9 +50,8 @@
    */
 import server from '@/libs/js/server'
 import cityUtil from '@/libs/js/city'
-import Emitter from 'iview/src/mixins/emitter'
 export default {
-  mixins: [Emitter],
+  name: 'SelectInputForCity',
   props: {
     autoFocus: {
       type: Boolean,
@@ -176,6 +175,21 @@ export default {
     }
   },
   methods: {
+    dispatch(componentName, eventName, params) {
+      let parent = this.$parent || this.$root;
+      let name = parent.$options.name;
+
+      while (parent && (!name || name !== componentName)) {
+        parent = parent.$parent;
+
+        if (parent) {
+          name = parent.$options.name;
+        }
+      }
+      if (parent) {
+        parent.$emit.apply(parent, [eventName].concat(params));
+      }
+    },
     onCompositionStart () {
       this.composing = true
     },
