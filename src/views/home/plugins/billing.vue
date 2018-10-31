8d1ba99c6eae7d682a755d464e56ca27cc1e4353
chentianwei <chentw@ymm56.com>
2018-10-31 15:12:25 +0800

    首页echarts颜色值

--- src/views/home/plugins/billing.vue
+++ src/views/home/plugins/billing.vue
@@ -105,7 +105,7 @@ export default {
                 x2: 0,
                 y2: 1,
                 colorStops: [{
-                  offset: 0, color: '#00a4bd69' // 0% 处的颜色
+                  offset: 0, color: '#00a4bd' // 0% 处的颜色
                 }, {
                   offset: 0.8, color: '#fff' // 100% 处的颜色
                 }]
