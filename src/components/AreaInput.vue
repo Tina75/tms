9d40182ed9da7f4bb25ecb370f8deac291275cf5
xubin <xpin_1509@163.com>
2018-10-31 13:49:00 +0800

    设置 地址搜索

--- src/components/AreaInput.vue
+++ src/components/AreaInput.vue
@@ -47,15 +47,15 @@ export default {
     areaList () {
       const obj = {}
       const res = []
-      const arr = this.localOptions.concat(this.address)
+      const arr = this.value ? this.address : this.localOptions
       for (let i = 0; i < arr.length; i++) {
         const item = arr[i]
-        if (!obj[item]) {
+        if (!obj[item.name]) {
           res.push(item)
-          obj[item] = 1
+          obj[item.name] = 1
         }
       }
-      return res.slice(0, 20)
+      return res
     },
     areaName () {
       const code = this.cityCode
