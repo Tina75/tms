13d5b6e8a723dbd6cd51fe196373372f60982b98
王军杰 <junjie.wang@ymm56.com>
2018-11-08 20:58:21 +0800

    UI

--- src/views/client/carrier-info.vue
+++ src/views/client/carrier-info.vue
@@ -152,7 +152,6 @@
                     :current.sync="pageNo1" :page-size-opts="pageArray1"
                     size="small"
                     show-sizer
-                    placement="top"
                     show-elevator show-total @on-change="handleChangePage1"
                     @on-page-size-change="handleChangePageSize1"/>
             </template>
@@ -199,7 +198,6 @@
                     :current.sync="pageNo2" :page-size-opts="pageArray2"
                     size="small"
                     show-sizer
-                    placement="top"
                     show-elevator show-total @on-change="handleChangePage2"
                     @on-page-size-change="handleChangePageSize2"/>
             </template>
