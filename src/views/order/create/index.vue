89b385b5172f9b86a61652ea63030b64e6aa286f
xubin <xpin_1509@163.com>
2018-11-29 17:24:53 +0800

    loading 位置

--- src/views/order/create/index.vue
+++ src/views/order/create/index.vue
@@ -1,8 +1,9 @@
 <template>
-  <Form ref="orderForm" :label-width="80" :model="orderForm" :rules="rules">
+  <Form ref="orderForm" :label-width="80" :model="orderForm" :rules="rules" style="position: relative;">
     <Spin v-if="loading" fix>
       <img src="../../../assets/loading.gif" width="24" height="24" alt="加载中">
     </Spin>
+
     <Row :gutter="16">
       <Col span="6">
       <FormItem label="客户名称:" prop="consignerName">
