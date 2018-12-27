1e0ad54e7427650f6d426699a19feae45dc80920
马友胜 <yousheng.ma@ymm56.com>
2018-12-27 16:51:48 +0800

    增加名称长度全局配置，并配置到vue实例中；修改收发货方联系号码

--- src/views/main/components/TabNavItem.vue
+++ src/views/main/components/TabNavItem.vue
@@ -5,7 +5,7 @@
       <span style="display:inline-block;min-width:18px">
         <Icon v-show="checked" class="tab-item__icon " type="ios-refresh" size="20" @click.stop="refresh"/>
       </span>
-      <router-link :to="path" :title="name" tag="span" class="tab-item__name">{{name}}</router-link>
+      <router-link :to="path" tag="span" class="tab-item__name">{{name}}</router-link>
       <span style="display:inline-block;min-width:18px">
         <Icon v-show="closeable" :style="checked?'visibility:visible':'visibility:hidden'" class="tab-item__icon close-icon" type="ios-close" size="20" @click.stop="close"/>
       </span>
@@ -36,7 +36,8 @@ export default {
       return {
         path: this.tab.path,
         query: {
-          title: this.tab.title,
+          // 点击标签会产生新的路由，关闭2次才可以
+          // title: this.tab.title,
           ...this.tab.query
         }
       }
