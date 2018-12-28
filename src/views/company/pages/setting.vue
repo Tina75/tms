04488fafe2aed54725fc0b4e19fffd0ca4b522b1
王军杰 <junjie.wang@ymm56.com>
2018-12-28 14:47:04 +0800

    UI

--- src/views/company/pages/setting.vue
+++ src/views/company/pages/setting.vue
@@ -557,9 +557,9 @@ export default {
 >>>.imageLogo .demo-upload-list
 >>>.imageLogo .ivu-upload .ivu-upload-drag
 >>>.imageLogo .ivu-upload .ivu-upload-input
-  width 96px
-  height 90px
 >>>.wxImaages .demo-upload-list
+>>>.wxImaages .ivu-upload .ivu-upload-drag
+>>>.wxImaages .ivu-upload-input
   width 96px
   height 90px
 >>>.wxImaages input.ivu-input.ivu-input-default
