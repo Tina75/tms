1b54dd21dea79c7dce977c930d8664ac06f86387
高婷 <ting.gao@ymm56.com>
2019-01-03 19:47:35 +0800

    ui调整

--- src/views/oilCard/constant/oil.js
+++ src/views/oilCard/constant/oil.js
@@ -109,7 +109,7 @@ export const oilTableColumns = vm => [
   {
     title: '操作',
     key: 'orderNo',
-    width: 120,
+    width: 125,
     // fixed: 'left',
     render: (h, params) => {
       let renderHtml = []
@@ -159,6 +159,7 @@ export const oilTableColumns = vm => [
           },
           [
             h('a', { style: { color: '#00A4BD' } }, '更多'),
+            h('Icon', { props: { type: 'ios-arrow-down' }, style: { color: '#00A4BD', marginLeft: '5px' } }),
             h('DropdownMenu', { slot: 'list' }, DropdownItem)
           ])
         )
